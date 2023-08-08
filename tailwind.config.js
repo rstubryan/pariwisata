60783db08a6279214c06017e686e3fe2618402ba
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-08-08 08:22:30 +0700

    revert ke cdn

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    "./index.html",
-  ],
-  plugins: [require("tw-elements/dist/plugin.cjs"), require("flowbite/plugin")],
-  darkMode: "class",
-};
