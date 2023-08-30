d626483a4a732af97f0fc8c5f04b2c398d17e36f
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-08-30 10:19:43 +0700

    penambahan faq ke 4

--- src/faq.js
+++ src/faq.js
@@ -153,6 +153,46 @@ function faq() {
             </p>
           </div>
         </div>
+        <h2 id="accordion-collapse-heading-4">
+          <button
+            type="button"
+            class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 hover:bg-gray-100"
+            data-accordion-target="#accordion-collapse-body-4"
+            aria-expanded="false"
+            aria-controls="accordion-collapse-body-4"
+          >
+            <span class="text-black"
+              >Apa saja fasilitas yang ada di Curug Cierang?</span
+            >
+            <svg
+              data-accordion-icon
+              class="w-3 h-3 rotate-180 shrink-0"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 5 5 1 1 5"
+              />
+            </svg>
+          </button>
+        </h2>
+        <div
+          id="accordion-collapse-body-4"
+          class="hidden"
+          aria-labelledby="accordion-collapse-heading-4"
+        >
+          <div class="p-5 border border-t-0 border-gray-200">
+            <p class="mb-2 text-gray-500">
+              Fasilitas yang ada di Curug Cierang ini berupa tempat parkir, WC, spot foto yang indah dan gazebo untuk tempat peristirahatan.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </section>
